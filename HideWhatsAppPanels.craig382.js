790189b9b2c1b1bfe0fbf107dd55bd19b8bde15d
craig382 <jazcfam@gmail.com>
2023-10-02 18:11:19 -0400

    Working version.

--- HideWhatsAppPanels.craig382.js
+++ HideWhatsAppPanels.craig382.js
@@ -2,7 +2,7 @@
 // @name         Hide WhatsApp Panels
 // @namespace    craig382
 // @version      0.1
-// @description  Add button and hotkey Ctrl + Shift + H to hide contact names on web.whatsapp.com
+// @description  Add button and hotkey Ctrl + Shift + H to hide side or main panel on web.whatsapp.com.
 // @author       Craig Veiner
 // @match        https://web.whatsapp.com/
 // @icon         https://i.imgur.com/LeZuNg7.png
@@ -10,17 +10,27 @@
 // ==/UserScript==
 
 // Based on Whatsapp Web Privacy Mode script by Graphen.
-
 /* jshint esversion: 6 */
+
 (function() {
     'use strict';
-    var hidden = false;
+    const sidePanelHeight = "45%";
+    const bothPanelsVisible = 0;
+    const mainPanelVisible = 1;
+    const sidePanelVisible = 2;
+    var visiblePanel = bothPanelsVisible;  // must equal 0, 1, or 2 (both, main, or side)
+    var topContainer1;
+    var topContainer2;
+    var sidePanel;
+    var mainPanel;
+    var bottomPanel;
+    var popOutPanel;
     var defaultbkg = 'lightgrey';
-    var prevName = '';
 
     waitForId('side').then(el => {
       //console.log(el);
     });
+
     function waitForId(id, nodeToObserve = document) {
       const el = document.getElementById(id);
       return el ?
@@ -32,11 +42,12 @@
               // add Ctrl + Shift + H shortcut
               document.addEventListener("keydown", function(e) {
                   if(e.ctrlKey && e.shiftKey && e.code === 'KeyH') {
-                      toggleShowHide();
+                      showPanel();
                   }
               }, false);
               // add Button
-              addButton('Hide', toggleShowHide);
+              initCss();
+              addButton('Both', showPanel);
               observer.disconnect();
               resolve(el);
             }
@@ -45,9 +56,11 @@
     }
 
     function addButton(text, onclick, cssObj) {
-        cssObj = cssObj || {position: 'absolute', top: '15px', left:'70px', 'z-index': 5000, 'font-weight':'bold', border:'black solid', 'border-radius':'10px',
-                            padding:'4px', 'background-color': defaultbkg, 'min-width': '75px', 'box-shadow':'grey 3px 3px 0px 0px'};
-        let button = document.createElement('button'), btnStyle = button.style;
+
+      cssObj = cssObj || {position: 'absolute', top: '15px', left:'70px', 'z-index': 5000, 'font-weight':'bold', border:'black solid', 'border-radius':'10px',
+                          padding:'4px', 'background-color': defaultbkg, 'min-width': '75px', 'box-shadow':'grey 3px 3px 0px 0px'};
+
+      let button = document.createElement('button'), btnStyle = button.style;
 
       try{
         document.getElementById('app').appendChild(button);
@@ -55,77 +68,97 @@
       catch(err){
         console.log("addButton: " + err);
       }
-        button.innerHTML = text;
-        button.onclick = onclick;
-        button.classList = ['show-hide-btn unpressed'];
 
-        button.onmouseover = function() {
-            button.style["background-color"] = 'salmon';
-        };
-        button.onmouseout = function() {
-            button.style["background-color"] = defaultbkg;
-        };
-        Object.keys(cssObj).forEach(key => btnStyle[key] = cssObj[key]);
-        return button;
+      button.innerHTML = text;
+      button.onclick = onclick;
+      button.classList = ['show-hide-btn unpressed'];
+      button.onmouseover = function() {
+        button.style["background-color"] = 'salmon';
+      };
+      button.onmouseout = function() {
+        button.style["background-color"] = defaultbkg;
+      };
+      Object.keys(cssObj).forEach(key => btnStyle[key] = cssObj[key]);
+      return button;
     }
 
-    function toggleShowHide() {
-        var btn = document.getElementsByClassName('show-hide-btn')[0];
-        var panel = document.getElementById('pane-side');
-        console.log("panel");
-        console.log(panel);
-        const side = document.getElementById('side');
-        console.log("side");
-        console.log(side);
-        const main = document.getElementById('main');
-        console.log("main");
-        console.log(main);
-//        const chatPanel = document.querySelector('#app > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(4)');
-        const chatPanel = document.querySelector('#app > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(1) > span');
-        console.log("chatPanel");
-        console.log(chatPanel); // looking for class="_2Ts6i _3RGKj _318SY"
-//        const defaultSideDisplay = side.style.display;
-//        var chatname = document.querySelector('#main > header > div:nth-of-type(2) > div:nth-of-type(1) > div > span');
-//        var groupsubheadline = document.querySelector('#main > header > div:nth-of-type(2) > div:nth-of-type(2) > span');
-//        var partnerimage = document.querySelector('#main > header > div:nth-of-type(1) > div:nth-of-type(1) > img');
-        //console.log(btn);
+    function initCss() {
+      topContainer1 = document.querySelector('._1jJ70');
+      console.log("topContainer1"); console.log(topContainer1);
+      topContainer2 = document.querySelector('._2QgSC');
+      console.log("topContainer2"); console.log(topContainer2);
+      sidePanel = document.getElementsByClassName('_3RGKj')[1];
+      console.log("sidePanel"); console.log(sidePanel);
+      mainPanel = document.getElementsByClassName('_2xAQV')[1];
+      console.log("mainPanel"); console.log(mainPanel);
+      bottomPanel = document.getElementsByClassName('_1xFRo')[0];
+      console.log("bottomPanel"); console.log(bottomPanel);
+      popOutPanel = document.getElementsByClassName('_3RGKj')[0];
+      console.log("popOutPanel"); console.log(popOutPanel);
 
-        if (hidden) {
-            // show
-            //console.log("WA Privacy: Toggled show.");
-//            chatPanel.setAttribute('style', 'filter:none');
-            chatPanel.style.width = "250px";
-//            side.style.width = "250px";
-//            main.style.marginLeft = "250px";
-//            side.style.width = "250px";
-//            side.setAttribute('style', 'filter:none');
-//            panel.setAttribute('style', 'filter:none');
-            defaultbkg = 'lightgrey';
-            btn.innerHTML = 'Hide';
-            btn.style.left = '70px';
-            btn.style.top = '15px';
-            btn.style["box-shadow"] = 'grey 3px 3px 0px 0px';
-            prevName = '';
-        } else {
-            // hide
-            //console.log("WA Privacy: Toggled hide.");
-//            panel.setAttribute('style', 'filter:blur(10px);');
-//            panel.setAttribute('style', 'display:none;');
-//            chatPanel.style.display = 'none';
-//            chatPanel.style.width = "0";
-//            side.style.display = 'none';
-//            side.style.width = "0";
-//            panel.style.display = 'none';
-            chatPanel.style.width = "0";
-//            side.style.width = "0";
-//            main.style.marginLeft = "0";
-            defaultbkg = 'grey';
-            btn.innerHTML = 'Show';
-            btn.style.left = '73px';
-            btn.style.top = '18px';
-            btn.style["box-shadow"] = '';
-        }
-        document.getElementsByClassName('show-hide-btn')[0].style["background-color"] = defaultbkg;
-        hidden = !hidden;
+      topContainer1.style.setProperty('flex-direction', 'column');
+      topContainer1.style.setProperty('min-width', '400px');
+
+      topContainer2.style.setProperty('flex-direction', 'column');
+      
+      sidePanel.style.setProperty('max-width', '100%');
+      sidePanel.style.setProperty('flex-basis', sidePanelHeight);
+      sidePanel.style.setProperty('width', '100%');
+      sidePanel.style.setProperty('height', sidePanelHeight);
+
+      mainPanel.style.setProperty('display', 'flex');
+      mainPanel.style.setProperty('flex-direction', 'column');
+
+      bottomPanel.style.setProperty('display', 'none');
+
+      popOutPanel.style.setProperty('max-width', '100%');
+      popOutPanel.style.setProperty('flex-basis', '100%');
+
+    }
+
+    function showPanel() {
+      var btn = document.getElementsByClassName('show-hide-btn')[0];
+      visiblePanel = (visiblePanel + 1) % 3;
+      // console.log("visiblePanel: " + visiblePanel);
+      switch(visiblePanel){
+        case bothPanelsVisible:
+          btn.innerHTML = 'Both';
+          btn.style["box-shadow"] = 'grey 3px 3px 0px 0px';
+          defaultbkg = 'lightgrey';
+          btn.style.left = '70px';
+          btn.style.top = '15px';
+          sidePanel.style.setProperty('height', sidePanelHeight);
+          sidePanel.style.setProperty('display', 'flex');
+          mainPanel.style.setProperty('display', 'flex');
+      break;
+        case mainPanelVisible:
+          btn.innerHTML = 'Main';
+          btn.style["box-shadow"] = '';
+          defaultbkg = 'grey';
+          btn.style.left = '73px';
+          btn.style.top = '18px';
+          sidePanel.style.setProperty('display', 'none');
+          mainPanel.style.setProperty('display', 'flex');
+          break;
+        case sidePanelVisible:
+          btn.innerHTML = 'Side';
+          btn.style["box-shadow"] = '';
+          defaultbkg = 'grey';
+          btn.style.left = '73px';
+          btn.style.top = '18px';
+          sidePanel.style.setProperty('display', 'flex');
+          mainPanel.style.setProperty('display', 'none');
+          sidePanel.style.setProperty('height', '100%');
+          break;
+        default:
+          console.log("WARNING. showPanel() switch default case exercised.");
+          break;
+      }
+      sidePanel.style.setProperty('max-width', '100%');
+      // popOutPanel.style.setProperty('max-width', '100%');
+      document.getElementsByClassName('show-hide-btn')[0].style["background-color"] = defaultbkg;
+      // console.log("sidePanel"); console.log(sidePanel);
+      // console.log("mainPanel"); console.log(mainPanel);
+
     }
 })();
